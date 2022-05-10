d81d5eca7fcdd06ea0fdcef9fe1a4d859e188367
daon1213 <diaeiyo1213@naver.com>
2022-05-10 14:36:57 +0900

    KakaoLocalApiService.kt commit!

---
+++ app/src/main/java/com/daon/finedust_part4_06/data/services/KakaoLocalApiService.kt
@@ -0,0 +1,18 @@
+package com.daon.finedust_part4_06.data.services
+
+import com.daon.finedust_part4_06.BuildConfig
+import com.daon.finedust_part4_06.data.models.tmcoordinates.TmCoordinatesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface KakaoLocalApiService {
+
+    @Headers("Authorization: KakaoAK ${BuildConfig.KAKAO_REST_API_KEY}")
+    @GET("v2/local/geo/transcoord.json?output_coord=TM")
+    suspend fun getTmCoordinates(
+        @Query("x") longitude: Double,
+        @Query("y") latitude: Double
+    ): Response<TmCoordinatesResponse>
+}
